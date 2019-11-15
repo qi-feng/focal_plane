281b59dc4e17b8a66c98d905291eef099ef0ab66
Qi Feng <sely520@gmail.com>
2019-11-14 21:38:08 -0700

    working version, trying to improve ring search

--- focal_plane.py
+++ focal_plane.py
@@ -839,8 +839,8 @@ def quick_check_raw_ring(rawfile,
     for i, row in df.iterrows():
         ax.annotate(row['Panel_ID_guess'], xy=np.array([row['X_IMAGE'], row['Y_IMAGE']]), size=8, xycoords='data',
                 # xytext=(np.array([row['X_IMAGE'] - 40, row['Y_IMAGE'] - 40])), # for orig lens
-                xytext=(np.array([row['X_IMAGE'] * 2.5 - center_pattern[0],
-                                  row['Y_IMAGE'] * 2.5 - center_pattern[1]])),  # for orig lens
+                xytext=(np.array([row['X_IMAGE'] + (row['X_IMAGE'] - center_pattern[0])*1.2,
+                                  row['Y_IMAGE'] + (row['Y_IMAGE'] - center_pattern[1])*1.2] )),  # for orig lens
                 # xytext=(np.array([row['X_IMAGE'] - 80, row['Y_IMAGE'] - 80])),  # for new lens
                 color='c', alpha=0.6,
                 arrowprops=dict(facecolor='c', edgecolor='c', shrink=0.05, headwidth=0.5, headlength=4, width=0.2,
